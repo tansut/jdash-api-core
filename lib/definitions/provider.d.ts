30cbd8469934fc102df556841edfb984bb0b30de
Tansu TURKOGLU <tansu@Tansus-MacBook-Pro.local>
2017-02-19 16:57:05 +0300

    initial commit

---
+++ lib/definitions/provider.d.ts
@@ -0,0 +1,12 @@
+/// <reference types="es6-shim" />
+import { Query, QueryResult, DashboardCreateModel, DashboardModel, CreateResult } from 'jdash-core';
+export interface ISearchDashboard {
+    appid?: string | Array<string>;
+    user?: string | Array<string>;
+    shareWith?: string | Array<string>;
+}
+export interface IDBProvider {
+    getDashboard?(appid: string, id: string): Promise<DashboardModel>;
+    searchDashboards(search: ISearchDashboard, query?: Query): Promise<QueryResult<DashboardModel>>;
+    createDashboard(model: DashboardCreateModel): Promise<CreateResult>;
+}
---
+++ src/provider.ts
@@ -0,0 +1,13 @@
+import { Query, QueryResult, DashboardCreateModel, DashboardModel, Metadata, CreateResult } from 'jdash-core';
+
+export interface ISearchDashboard {
+    appid?: string | Array<string>;
+    user?: string | Array<string>;
+    shareWith?: string | Array<string>;
+}
+
+export interface IDBProvider {
+    getDashboard?(appid: string, id: string): Promise<DashboardModel>;
+    searchDashboards(search: ISearchDashboard, query?: Query): Promise<QueryResult<DashboardModel>>;
+    createDashboard(model: DashboardCreateModel): Promise<CreateResult>;
+}
