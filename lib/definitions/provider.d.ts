7b61c1bac0e318b4db6393364d9ed4e6bb11f9a0
Tansu TURKOGLU <tansu@Tansus-MacBook-Pro.local>
2017-02-20 12:03:36 +0300

    removed es6 shim

--- lib/definitions/provider.d.ts
+++ lib/definitions/provider.d.ts
@@ -1,9 +1,13 @@
-/// <reference types="es6-shim" />
-import { Query, QueryResult, DashboardCreateModel, DashboardUpdateModel, DashboardModel, CreateResult } from 'jdash-core';
+/// <reference types="es6-promise" />
+import { Query, QueryResult, DashboardCreateModel, DashboardUpdateModel, DashletCreateModel, DashletModel, DashletUpdateModel, DashboardModel, CreateResult } from 'jdash-core';
 export interface ISearchDashboard {
     appid?: string | Array<string>;
     user?: string | Array<string>;
     shareWith?: string | Array<string>;
+}
+export interface ISearchDashlet {
+    user?: string | Array<string>;
+    dashboardId?: string;
 }
 export interface IDBProvider {
     getDashboard?(appid: string, id: string): Promise<DashboardModel>;
@@ -11,4 +15,8 @@ export interface IDBProvider {
     createDashboard(appid: string, model: DashboardCreateModel): Promise<CreateResult>;
     deleteDashboard(appid: string, id: string): Promise<any>;
     updateDashboard(appid: string, id: string, updateValues: DashboardUpdateModel): Promise<any>;
+    createDashlet(model: DashletCreateModel): Promise<CreateResult>;
+    searchDashlets(search: ISearchDashlet): Promise<Array<DashletModel>>;
+    deleteDashlet(appid: string, id: string): Promise<any>;
+    updateDashlet(id: string, updateValues: DashletUpdateModel): Promise<any>;
 }
--- src/provider.ts
+++ src/provider.ts
@@ -1,9 +1,14 @@
-import { Query, QueryResult, DashboardCreateModel, DashboardUpdateModel, DashboardModel, Metadata, CreateResult } from 'jdash-core';
+import { Query, QueryResult, DashboardCreateModel, DashboardUpdateModel, DashletCreateModel, DashletModel, DashletUpdateModel, DashboardModel, CreateResult } from 'jdash-core';
 
 export interface ISearchDashboard {
     appid?: string | Array<string>;
     user?: string | Array<string>;
     shareWith?: string | Array<string>;
+}
+
+export interface ISearchDashlet {
+    user?: string | Array<string>;
+    dashboardId?: string;
 }
 
 export interface IDBProvider {
@@ -12,4 +17,10 @@ export interface IDBProvider {
     createDashboard(appid: string, model: DashboardCreateModel): Promise<CreateResult>;
     deleteDashboard(appid: string, id: string): Promise<any>;
     updateDashboard(appid: string, id: string, updateValues: DashboardUpdateModel): Promise<any>;
+
+    createDashlet(model: DashletCreateModel): Promise<CreateResult>;
+    searchDashlets(search: ISearchDashlet): Promise<Array<DashletModel>>;
+    deleteDashlet(appid: string, id: string): Promise<any>;
+    updateDashlet(id: string, updateValues: DashletUpdateModel): Promise<any>;
+
 }
