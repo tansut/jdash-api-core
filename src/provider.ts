8f9cb117cc564d584c782051744b97ec317bf256
Tansu TURKOGLU <tansu@Tansus-MacBook-Pro.local>
2017-02-21 17:59:38 +0300

    .

--- src/provider.ts
+++ src/provider.ts
@@ -14,11 +14,11 @@ export interface ISearchDashlet {
 export interface IDBProvider {
     getDashboard?(appid: string, id: string): Promise<GetDashboardResult>;
     searchDashboards(search: ISearchDashboard, query?: Query): Promise<QueryResult<DashboardModel>>;
-    createDashboard(appid: string, model: DashboardCreateModel): Promise<CreateResult>;
+    createDashboard(appid: string, model: DashboardModel): Promise<CreateResult>;
     deleteDashboard(appid: string, id: string): Promise<any>;
     updateDashboard(appid: string, id: string, updateValues: DashboardUpdateModel): Promise<any>;
 
-    createDashlet(model: DashletCreateModel): Promise<CreateResult>;
+    createDashlet(model: DashletModel): Promise<CreateResult>;
     searchDashlets(search: ISearchDashlet): Promise<Array<DashletModel>>;
     deleteDashlet(id: string): Promise<any>;
     updateDashlet(id: string, updateValues: DashletUpdateModel): Promise<any>;
